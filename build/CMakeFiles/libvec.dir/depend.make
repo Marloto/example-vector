99356234b854e33dd8c739b184737626dab187ee
Marloto <marloto@codestack.de>
2022-11-07 10:24:19 +0100

    added branch with lib

---
+++ build/CMakeFiles/libvec.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for libvec.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/vector.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/seb/Entwicklung/THI/Repositories/swmgt/ws2022/example-vector/main.cpp" "CMakeFiles/vector.dir/main.cpp.o" "gcc" "CMakeFiles/vector.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/seb/Entwicklung/THI/Repositories/swmgt/ws2022/example-vector/build/CMakeFiles/libvec.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
